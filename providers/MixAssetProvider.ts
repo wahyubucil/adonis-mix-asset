9b765023423383c952361213f1fe7a3128807754
Harminder Virk <virk.officials@gmail.com>
2020-10-28 10:41:38 +0530

    refactor: use `readFileSync` over `require`
    The require method caches the required module and hence the changes made to the manifest
    file will not be picked by the process until it re-starts

--- providers/MixAssetProvider.ts
+++ providers/MixAssetProvider.ts
@@ -1,6 +1,6 @@
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import { ViewContract } from '@ioc:Adonis/Core/View'
-import { existsSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 import { mixAsset } from '../src/mixAsset'
 
 export default class MixAssetProvider {
@@ -11,14 +11,27 @@ export default class MixAssetProvider {
 		// Register your own bindings
 	}
 
+	/**
+	 * Returns the manifest file contents. During development, we make use of the
+	 * `readFileSync` method to read the fresh contents on every call
+	 */
+	private getManifestContents(manifestPath: string) {
+		if (this.app.inProduction) {
+			return require(manifestPath)
+		}
+
+		JSON.parse(readFileSync(manifestPath, 'utf-8'))
+	}
+
 	public boot() {
 		// IoC container is ready
 		this.app.container.with(['Adonis/Core/View'], (view: ViewContract) => {
 			// Load manifest on boot. So it doesn't load every mixAsset function
 			const manifestPath = this.app.publicPath('mix-manifest.json')
 			if (existsSync(manifestPath)) {
-				const manifest = require(manifestPath) as Record<string, string>
-				view.global('mix', (path: string) => mixAsset(this.app, manifest, path))
+				view.global('mix', (path: string) =>
+					mixAsset(this.app, this.getManifestContents(manifestPath), path)
+				)
 			}
 		})
 	}
