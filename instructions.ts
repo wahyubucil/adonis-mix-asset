9ef4bbb08d69418e4a2e45e933dcc906b3654bdf
Wahyu Bucil <wahyubucil@gmail.com>
2021-03-11 12:16:20 +0800

    fix: linter issue
    Fix linter issue because eslint-config-prettier v8.0.0 changes
    (https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21)

--- instructions.ts
+++ instructions.ts
@@ -5,15 +5,15 @@ import { join } from 'path'
 const MIX_CONFIG_TEMPLATE_STUB = join(__dirname, './templates', 'webpack.mix.txt')
 
 export default async function instructions(
-	projectRoot: string,
-	_: ApplicationContract,
-	{ logger, files }: typeof sinkStatic
+  projectRoot: string,
+  _: ApplicationContract,
+  { logger, files }: typeof sinkStatic
 ) {
-	const mixConfig = new files.MustacheFile(projectRoot, 'webpack.mix.js', MIX_CONFIG_TEMPLATE_STUB)
-	if (mixConfig.exists()) {
-		logger.action('create').skipped('webpack.mix.js', 'File already exists')
-	} else {
-		mixConfig.apply({}).commit()
-		logger.action('create').succeeded('webpack.mix.js')
-	}
+  const mixConfig = new files.MustacheFile(projectRoot, 'webpack.mix.js', MIX_CONFIG_TEMPLATE_STUB)
+  if (mixConfig.exists()) {
+    logger.action('create').skipped('webpack.mix.js', 'File already exists')
+  } else {
+    mixConfig.apply({}).commit()
+    logger.action('create').succeeded('webpack.mix.js')
+  }
 }
